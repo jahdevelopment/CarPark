f7ba9b51ce10fed93b259a26691bad609ff9846d
juanhernandezdeveloper <jualherac@gmail.com>
2023-01-19 19:26:53 -0600

    Last commit

--- Program.cs
+++ Program.cs
@@ -16,39 +16,87 @@ Users enter a space number. If that number is "full", the console displays a war
 
  */
 
-
 Console.WriteLine("================= CAR PARK =================");
 
-Console.WriteLine("Please enter the number of spots for the car park:");
-int totalSpaces = Int32.Parse(Console.ReadLine());
+char[] letters = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'X', 'Y', 'Z' };
 
-Dictionary<List<string>, List<string>> carPark = 
-    new Dictionary<List<string>, List<string>>();
+Console.WriteLine("Please enter how many spaces you want to add (maximum of 260)");
 
-List<string> carSpots = new List<string>();
+int parkSize = Int32.Parse(Console.ReadLine());
 
-List<string> stateSpot = new List<string>();
-
-char[] letters = new char[25]
-    { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z' };
-
-char[] ints = new char[10] {'0','1','2','3','4','5','6','7','8','9'};
-
-for (int i = 0; i <= totalSpaces -1; i++)
+while(parkSize < 0 || parkSize > 260)
 {
-    if (totalSpaces < 10)
-    {
-        carSpots.Add($"{letters[1]}{ints[i]}");
+    Console.WriteLine("Please enter a value between 0 and 261");
 
-        Console.WriteLine(carSpots[i]);
-    }
-    else if (totalSpaces >= 10) { }
-    {
-        carSpots.Add($"{letters[0]}{ints[i]}");
-        carSpots.Add($"{letters[1]}{ints[i]}");
-        Console.WriteLine(carSpots[i]);
-    }
-
-    
+    parkSize = Int32.Parse(Console.ReadLine());
 }
+
+Dictionary<string, string> carPark = new Dictionary<string, string>();
+
+for(int i = 0;i < parkSize; i++)
+{
+    char letter = letters[i / 10];
+
+    int number = i % 10;
+
+    string parkingSpot = $"{letter}{number}";
+
+    carPark.Add(parkingSpot, "");
+}
+
+foreach(string parkingSpot in carPark.Keys)
+{
+    Console.WriteLine(parkingSpot);
+}
+// get a parking spot
+
+Console.WriteLine($"Enter a spot you would like to park in between {carPark.Keys.First()} and {carPark.Keys.Last()}, or LIST to see the list of spots");
+
+string requestedInput = Console.ReadLine().ToUpper();
+
+if (carPark.ContainsKey(requestedInput))
+{
+    if(requestedInput == "LIST")
+    {
+        foreach(KeyValuePair<string, string> pair in carPark)
+        {
+            string parkedCarDisplay = pair.Value;
+
+            if (string.IsNullOrEmpty(parkedCarDisplay))
+            {
+                parkedCarDisplay = "EMPTY";
+            }
+            Console.WriteLine($"{pair.Key}: {parkedCarDisplay}");
+        }
+    }
+    else if (string.IsNullOrEmpty(carPark[requestedInput]))
+    {
+        Console.WriteLine("Please enter your vehicle's license");
+
+        string licenseInput = Console.ReadLine();
+
+        while (licenseInput.Length < 1 || licenseInput.Length > 7)
+        {
+            // === INVALID LICENSE ===
+            Console.WriteLine("Sorry, that is not a valid license. Please enter a valid license");
+            licenseInput = Console.ReadLine();
+        }
+
+        // === SUCCESFULL ADDITION ===
+        carPark[requestedInput] = licenseInput;
+
+        Console.WriteLine($"Succesfully registered {carPark[requestedInput]} with spot {requestedInput}.");
+    }
+    else
+    {
+        // === SPOT TAKEN ===
+        Console.WriteLine($"Sorry, {requestedInput} is not available.");
+    }
+}
+else
+{
+    // === INVALID SPOT ===
+    Console.WriteLine($"Sorry, {requestedInput} is not a valid parking spot.");
+}
+
 
